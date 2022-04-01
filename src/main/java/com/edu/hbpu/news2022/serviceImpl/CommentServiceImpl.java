3df9ef52641560fecafb44a20e775b959e690298
cherryxiong <xio2k@foxmail.com>
2022-04-01 12:40:26 +0800

    4.1

--- src/main/java/com/edu/hbpu/news2022/serviceImpl/CommentServiceImpl.java
+++ src/main/java/com/edu/hbpu/news2022/serviceImpl/CommentServiceImpl.java
@@ -19,7 +19,7 @@ import java.util.List;
  */
 @Service
 public class CommentServiceImpl extends ServiceImpl<CommentMapper, Comment> implements CommentService {
-    //a
+
     @Autowired
     CommentMapper commentMapper;
     @Override
