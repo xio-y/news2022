3e2ecdb1cd5750e554b7c9aa68544dfb254e7220
cherryxiong <xio2k@foxmail.com>
2022-04-17 20:09:45 +0800

    =

--- src/test/java/com/edu/hbpu/news2022/News2022ApplicationTests.java
+++ src/test/java/com/edu/hbpu/news2022/News2022ApplicationTests.java
@@ -2,30 +2,10 @@ package com.edu.hbpu.news2022;
 
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
 
 
 @SpringBootTest
 class News2022ApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-    private static final String FOllOW_USER_KEY="FOLLOW_USER_KEY_";
-    @Autowired
-    private RedisTemplate redisTemplate;
-    private String getFollowUserKey(Long userId){
-        return this.FOllOW_USER_KEY+userId;
-    }
-    @Test
-    public void test(){
-        redisTemplate.opsForHash().put("as","a","b");
-        redisTemplate.opsForHash().put(this.getFollowUserKey(new Long(5)),3+"",1);
-        System.out.println(this.redisTemplate.opsForHash().hasKey("as","a"));
-    }
-
 
 }
